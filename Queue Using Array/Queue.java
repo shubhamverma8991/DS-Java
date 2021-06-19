eee3f39755dfec4ba54fc8ce1f9664b63bdd9098
Shubham <66171089+shubhamverma8991@users.noreply.github.com>
2021-06-20 01:08:50 +0530

    using Java

--- Queue Using Array/Queue.java
+++ Queue Using Array/Queue.java
@@ -1,35 +1,58 @@
-package com.queue;
+package com.cdac;
 
 public class Queue {
+	private int front;
+	private int rear;
 	private int count;
-	private int[] arr ;
-	private int size,front, rear;
+	private int[] arr;
 
+	/*
+	 * public Queue() { front = 0; rear = -1; count = 0; arr = new int[5]; }
+	 */
 	public Queue(int size) {
-		this.size = size;
-		arr=new int[size];
-		rear = 0;
 		front = 0;
+		rear = -1;
+		count = 0;
+		arr = new int[size];
 	}
 
-	public void enQueue(int value) {
-		arr[rear] = value;
-		rear++;
-		count++;
+	public boolean isFull() {
+		return (count == arr.length);
 	}
 
-	public int deQueue() {
-		int data = arr[front];
-		front++;
-		count--;
-		return data;
+	public boolean isEmpty() {
+		return (count == 0);
+	}
 
+	public void enqueue(int element) {
+		if (isFull()) {
+			System.out.println("Queue is full.");
+		} else {
+			rear = (rear + 1) % arr.length;
+			arr[rear] = element;
+			count++;
+		}
+	}
+
+	public int dequeue() {
+		if (isEmpty()) {
+			throw new RuntimeException("Queue is empty.");
+		} else {
+			int temp = arr[front];
+			front = (front + 1) % arr.length;
+			count--;
+			return temp;
+		}
 	}
 
 	public void show() {
-		System.out.println("Elements :");
-		for (int i = 0; i < count; i++) {
-			System.out.print(arr[i] + " ");
+		if (isEmpty()) {
+			throw new RuntimeException("Queue is Empty");
+		} else {
+			System.out.println("Elements: ");
+			for (int i = 0; i < count; i++) {
+				System.out.println(arr[(front + i) % 5]);
+			}
 		}
 	}
 }
--- Queue Using Array/Runner.java
+++ Queue Using Array/Runner.java
@@ -1,15 +1,44 @@
-package com.queue;
+package com.cdac;
 
 public class Runner {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Queue q=new Queue(100);
-		q.enQueue(10);
-		q.enQueue(20);
-		q.enQueue(30);
-		
-		q.show();
+		Queue q = new Queue(5);
+		try {
+			q.enqueue(10);
+			q.enqueue(20);
+			q.enqueue(30);
+
+			q.show();
+			System.out.println();
+			System.out.println("deQueue Elememts are : ");
+
+			System.out.print(q.dequeue() + " ");
+			System.out.print(q.dequeue() + " ");
+
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println("Remaining elements are:");
+			q.show();
+
+			q.enqueue(40);
+			q.enqueue(50);
+			q.enqueue(60);
+			q.enqueue(70);
+			q.enqueue(80);
+			System.out.println("Show");
+			q.show();
+			// Empty all Queue
+
+			while (!q.isEmpty()) {
+				System.out.println(q.dequeue());
+			}
+			System.out.println("after Empty");
+			q.show();
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
 	}
 
 }
